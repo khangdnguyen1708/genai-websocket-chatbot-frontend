93f084484e622101a95a339c2f6845700feac4fc
Quang Mau <mau@vntechies.dev>
2024-07-17 13:28:18 +0700

    added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,13 @@ const Footer = () => {
     return (
         <footer className="rounded-lg shadow m-4">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center ">© 2024 <a href="https://flowbite.com/" className="hover:underline">Cloud Kinetics</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center ">© 2024 <a href="https://www.cloud-kinetics.com/" className="hover:underline">Cloud Kinetics</a>. All Rights Reserved.
                 </span>
+
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+                    <li>
+                        <a href="https://www.cloud-kinetics.com/services/applications-software-engineering/app-modernization/" className="hover:underline me-4 md:me-6">CKVN SA Team with ❤️</a>
+                    </li>
                     <li>
                         <a href="https://www.cloud-kinetics.com/services/cloud-solutions/cloud-migration/" className="hover:underline me-4 md:me-6">Cloud Migration</a>
                     </li>
@@ -12,16 +16,10 @@ const Footer = () => {
                         <a href="https://www.cloud-kinetics.com/services/applications-software-engineering/app-modernization/" className="hover:underline me-4 md:me-6">App Modernization</a>
                     </li>
                     <li>
-                        <a href="https://www.cloud-kinetics.com/services/applications-software-engineering/devops/" className="hover:underline me-4 md:me-6">DevSecOps</a>
-                    </li>
-                    <li>
                         <a href="https://www.cloud-kinetics.com/privacy-policy/" className="hover:underline me-4 md:me-6">Privacy Policy</a>
                     </li>
                     <li>
                         <a href="https://www.cloud-kinetics.com/terms-and-conditions/" className="hover:underline me-4 md:me-6">Terms and Conditions</a>
-                    </li>
-                    <li>
-                        <a href="https://www.cloud-kinetics.com/information-security-policy/" className="hover:underline">Information Security Policy</a>
                     </li>
                 </ul>
             </div>
